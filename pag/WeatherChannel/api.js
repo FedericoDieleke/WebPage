c79b57663eb2613002b017e7efbe4168f9d58bd0
FedericoDieleke <federicodieleke@hotmail.com>
2023-11-17 21:45:22 -0300

    Error Managment for api error

--- pag/WeatherChannel/api.js
+++ pag/WeatherChannel/api.js
@@ -4,11 +4,12 @@ let ciudad;
 
 
 function cargarCiudad(ciudad){
-  let url = "https://api.openweathermap.org/data/2.5/weather?q="+ciudad+"&appid=dcec7df661b1e6b0edab51d796b7339c&units=metric&lang=es";
-  let iconoURL = "https://openweathermap.org/img/wn/"
-  $.getJSON(url
-    , 
-    function (data) {
+   let iconoURL = "https://openweathermap.org/img/wn/"
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/weather?q="+ciudad+"&appid=dcec7df661b1e6b0edab51d796b7339c&units=metric&lang=es",
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
         console.log(data); 
         document.querySelector(".container").style.visibility = "visible";
         document.querySelector("#ciudad").textContent = data.name;
@@ -16,10 +17,17 @@ function cargarCiudad(ciudad){
         document.querySelector("#grados").textContent ="°C";
         document.querySelector("img").src = iconoURL+data.weather[0].icon+"@2x.png";
         document.querySelector("#descripcion").textContent = data.weather[0].description;
-
-    });
+    },
+    error: function (response) {
+        console.log(response);
+        alert("Error en el nombre de la Ciudad");
+    }
+})
 
 };
+
+
+
 
 function click(){
   let limpio; // Variable para cambiar los espacios por codigo html %20
@@ -27,7 +35,12 @@ function click(){
   document.getElementById("boton").addEventListener("click", function(){
       limpio = document.getElementById("entrada").value.replace(" ","%20");// cambia los espacios por codigo html
       document.getElementById("entrada").value = ""; //Borra el texto ingresado
-      cargarCiudad(limpio);//Ejecuta la api con el nombre de la ciudad formateado
+        try {
+          cargarCiudad(limpio);//Ejecuta la api con el nombre de la ciudad formateado
+        } catch (error) {
+          console.error(error);
+          alert("Ciudad Vacia!");
+        }
   })
 }
 
